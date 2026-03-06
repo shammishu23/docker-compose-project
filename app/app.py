b744cbfd9f55bf6422a9e2ba895ed6ce3058ed3b
sujitha <chadalavadasujitha8@gmail.com>
2026-03-06 20:18:45 +0530

    modified the app.py and Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,4 +1,13 @@
-with open("/data/log.txt", "a") as f:
-     f.write("Devops Day 8 learning\n")
+#with open("/data/log.txt", "a") as f:
+ #    f.write("Devops Day 8 learning\n")
 
-print("Message written to volume")
+#print("Message written to volume")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from DevOps pipeline deployed on AWS!"
+
+app.run(host="0.0.0.0", port=5000)
